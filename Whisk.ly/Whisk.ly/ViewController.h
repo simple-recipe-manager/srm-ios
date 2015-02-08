2b59ef3403b79f2df713fda0aa58685a8dd00025
Breland Miley <breland@brelands-mac.local>
2015-02-08 00:29:26 -0800

    Initial commit

---
+++ Whisk.ly/Whisk.ly/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Whisk.ly
+//
+//  Created by Breland Miley on 2/8/15.
+//  Copyright (c) 2015 Whisk.ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Whisk.ly/Whisk.ly/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Whisk.ly
+//
+//  Created by Breland Miley on 2/8/15.
+//  Copyright (c) 2015 Whisk.ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
